bb9696dd25810e0e0beb40e9d353f06423baf670
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-08-05 00:02:48 -0300

    chore: added nunito google font to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,9 @@ import AppRoleNavbarLogo from '../assets/approle_logo_navbar.png';
 
 export function Navbar() {
   return (
-    <div className="w-full h-14 bg-transparent flex rounded-b-xl">
+    <div className="w-full h-14 bg-[rgba(29, 29, 29, 0.4)] flex rounded-b-xl">
       <img src={AppRoleNavbarLogo} alt="AppRole Logo" className="h-10 m-2 mr-44" />
-      <div className='flex m-4 justify-around w-8/12'>
+      <div className='flex m-4 justify-around w-8/12 font-nunito'>
         <h1 className="text-lg font-light text-white cursor-pointer">Quem Somos</h1>
         <h1 className="text-lg font-light text-white cursor-pointer">Benefícios</h1>
         <h1 className="text-lg font-light text-white cursor-pointer">Organizador</h1>
