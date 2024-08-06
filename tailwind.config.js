bace7cc15f14d3542e387713d97eec27eacf4f9c
ZettDev <131803879+ZettDev@users.noreply.github.com>
2024-08-06 17:42:25 -0300

    theme colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white-purple': '#F2E3FC',
+        'violet': '#9C4EDC',
+        'light-purple': '#5A189A',
+        'purple': '#3C096C',
+        'dark-purple': '#10002B',
+      },
+    }
   },
   plugins: [],
 }
