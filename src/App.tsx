4405538aff56180adb6858b562bdf1beaa9bccbf
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-08-06 00:47:32 -0300

    added componentizations, added new divs to separate in better way the containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import Spline from "@splinetool/react-spline";
-import dash from './assets/Dashboard_1.png';
+import { Contact } from "./components/Contact";
+import { Header } from "./components/Header";
+import { SectionContainer } from "./components/SectionContainer";
 
 
 export function App() {
@@ -7,40 +8,12 @@ export function App() {
     <>
       <main>
         {/* SECTION 1 - 3D */}
-        <section className="min-h-screen flex justify-around items-center py-16 text-white bg-gradient-to-r from-purple-950 to-purple-600 max-lg:flex-col">
-          <div className="w-1/3 flex justify-center flex-col gap-8 lg:ml-8 max-lg:w-[75%]">
-            <h1 className="text-6xl max-lg:text-center max-lg:text-5xl">O seu <span className="uppercase font-bold text-purple-600">role</span> ideal está aqui!</h1>
-
-            <p className="text-xl font-light max-lg:text-center">O app ROLE está vindo com tudo, encontre novas pessoas e descubra todos os eventos noturnos na sua cidade!</p>
-            <p className="text-xl font-light max-lg:text-center">Tudo de acordo com as suas preferências e gostos, aproveitando do melhor que a vida noturna tem a oferecer!</p>
-
-            <div className="flex gap-4 text-4xl max-lg:hidden">
-              <i className="cursor-pointer pi pi-instagram"></i>
-              <i className="cursor-pointer pi pi-tiktok"></i>
-              <i className="cursor-pointer pi pi-twitter"></i>
-            </div>
-
-            <p className="max-lg:hidden">Acesse nossas redes sociais!</p>
-          </div>
-          <div className="w-1/2 flex justify-center relative max-lg:w-full max-lg:hidden">
-            <img src={dash} alt="Imagem de dashboard"/>
-            <Spline className="absolute top-0 pointer-events-auto"
-              scene="https://prod.spline.design/YsywGClJ43grZrVj/scene.splinecode"
-            />
-          </div>
-
-          <div className="flex items-end p-8">
-            <Spline
-              className="hidden max-lg:block"
-              scene="https://prod.spline.design/YsywGClJ43grZrVj/scene.splinecode"
-            />
-            <div className="flex flex-col gap-4 text-4xl lg:hidden">
-              <i className="cursor-pointer pi pi-instagram"></i>
-              <i className="cursor-pointer pi pi-tiktok"></i>
-              <i className="cursor-pointer pi pi-twitter"></i>
-            </div>
-          </div>
-        </section>
+        <SectionContainer>
+          <Header />
+        </SectionContainer>
+        <SectionContainer>
+          <Contact />
+        </SectionContainer>
       </main>
     </>
   )
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,31 +1,48 @@
-import { Checkbox } from "./Checkbox";
+import { Checkbox } from "primereact/checkbox";
+import { useState } from "react";
 
         
 
 export function Contact() {
+    const [checked, setChecked] = useState(false);
+
     return (
         <>
-        <section className="bg-gradient-to-r from-purple-950 to-purple-600 text-white flex justify-center items-center h-screen">
-            <div className="bg-[#1D1D1D] bg-opacity-40 h-[453px] w-11/12 rounded-[50px]">
-                <h1 className="text-5xl ml-[250px] mt-[51px]">Fale Conosco</h1>
-                <div className="flex">
-                    <div className="mt-[22px]">
-                        <p className="ml-[68px] mb-[7px] text-2xl">Nome:</p>
-                        <input type="" className="bg-transparent border-opacity-60 border-2 border-white rounded-[15px] placeholder-[#838383] h-[48px] w-[338px] pl-[14px] ml-[62px]" placeholder="Digite seu nome..."/>
+        <section className="bg-transparent text-white flex justify-center items-center h-[80%] w-[92%]">
+            <div className="bg-[#1D1D1D] bg-opacity-40 h-full w-full rounded-[50px] flex">
+                <div id="container_esquerda" className="flex flex-col">
+                    <h1 className="text-5xl ml-[250px] mt-[51px]">Fale Conosco</h1>
+                    <div className="flex flex-col">
+                        <div className="mt-[22px]">
+                            <p className="ml-[68px] mb-[7px] text-2xl">Nome:</p>
+                            <input type="" className="bg-transparent border-opacity-60 border-2 border-white rounded-[15px] placeholder-[#838383] h-[48px] w-[338px] pl-[14px] ml-[62px]" placeholder="Digite seu nome..."/>
+                        </div>
+
+                        <div className="mt-[23px]">
+                            <p className="ml-[68px] mb-[7px] text-2xl">E-mail:</p>
+                            <input type="" className="bg-transparent border-opacity-60 border-2 border-white rounded-[15px] placeholder-[#838383] h-[48px] w-[338px] pl-[14px] ml-[62px]" placeholder="Digite seu e-mail..."/>
+                        </div>
+
+                        <div>
+                            <Checkbox className="border-white bg-transparent w-11 h-11" checked={checked} onChange={(e) => {e.checked ? setChecked(e.checked) : null}}>
+                                <div className="ml-[41px] text-2xl text-white">Organizador</div>
+                            </Checkbox>
+                        </div>
                     </div>
+                </div>
+                <div id="container_direita">
+                </div>
+                {/* <h1 className="text-5xl ml-[250px] mt-[51px]">Fale Conosco</h1>
+                <div className="flex">
                     <div className="ml-[29px] mt-[22px]">
                         <p className="ml-[68px] mb-[7px] text-2xl">Escreva sua mensagem:</p>
                         <textarea id="" rows={10} cols={10} className="bg-transparent w-[437px] border-opacity-60 border-2 border-white rounded-[15px] placeholder-[#838383] pl-[14px] pt-[10px]" placeholder="Digite sua mensagem..."/>
                     </div>
                 </div>
-                <div className="mt-[23px]">
-                    <p className="ml-[68px] mb-[7px] text-2xl">E-mail:</p>
-                    <input type="" className="bg-transparent border-opacity-60 border-2 border-white rounded-[15px] placeholder-[#838383] h-[48px] w-[338px] pl-[14px] ml-[62px]" placeholder="Digite seu e-mail..."/>
-                </div>
                 <div className="flex">
                     <Checkbox />
                     <button className="bg-gradient-to-r from-[#5A189A] to-[#3C096C] ml-[684px] w-[182px] h-[37px] border-1 rounded-[15px] text-2xl mt-[47px] shadow-black shadow-2xl">Enviar</button>
-                </div>
+                </div> */}
             </div>   
         </section>
         </>
