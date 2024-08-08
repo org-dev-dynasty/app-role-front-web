33d89e5acd15c680dc5dfd90d62ada85de4354f8
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-08-07 23:16:25 -0300

    feat: Update NavbarMenu styling
    The NavbarMenu component's styling has been updated to use a new background color and apply a backdrop blur effect. This change improves the visual appearance of the navbar menu.

--- src/components/NavbarMenu.tsx
+++ src/components/NavbarMenu.tsx
@@ -1,9 +1,7 @@
 import { NavbarItem } from "./NavbarItem";
 
 export function NavbarMenu() {
-  return <div className="
-    bg-[rgba(87,87,87,0.8)] backdrop-blur-3xl w-[40%] h-[60vh] fixed top-0 right-0 z-100 flex flex-col justify-around items-center
-  ">
+  return <div className="bg-[rgba(29,29,29,0.4)] w-[40%] h-[60vh] fixed top- right-0 z-1000 flex flex-col justify-around items-center backdrop-blur">
     <NavbarItem className="">Quem Somos</NavbarItem>
     <NavbarItem>Benefícios</NavbarItem>
     <NavbarItem>Organizador</NavbarItem>
