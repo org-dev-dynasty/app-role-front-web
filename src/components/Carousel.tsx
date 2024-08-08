16db88535c966fdd045cf01e68fbdde78ba848e0
ZettDev <23.00171-2@maua.br>
2024-08-08 11:29:03 -0300

    Interactable carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -4,6 +4,7 @@ import mock from '../assets/Home 3 ESCULRO-portrait.png';
 import mock2 from '../assets/Filtros ESCURO-portrait.png';
 import mock3 from '../assets/Perfil escuro-portrait.png';
 import mock4 from '../assets/Perfil Org ESCURO1-portrait.png';
+import { Image } from 'primereact/image';
 
 const images = [
     mock,
@@ -13,9 +14,8 @@ const images = [
 ];
 
 export const Carousel = ({ currentIndex, prevSlide, nextSlide }) => {
-
     return (
-        <div className="relative w-5/6 flex justify-center items-center">
+        <div className="relative w-full flex justify-center items-center">
             <button
                 onClick={prevSlide}
                 className="absolute left-0 transform -translate-y-1/2 text-light-purple mx-4"
@@ -23,21 +23,36 @@ export const Carousel = ({ currentIndex, prevSlide, nextSlide }) => {
                 <CaretCircleLeft size={36} />
             </button>
 
-            <div className="w-[250px] overflow-hidden">
+            <div className='w-1/6'/>
+
+            <div className="relative w-[250px] overflow-hidden">
                 <div
                     className="flex transition-transform duration-500"
-                    style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+                    style={{ transform: `translateX(-${currentIndex * 100}%)`}}
                 >
-                    {images.map((image: string, index) => (
-                        <img
-                            key={index}
-                            src={image}
-                            alt={`Slide ${index}`}
-                            className="w-full object-cover"
-                        />
+                    {images.map((image, index) => (
+                        <div key={index} className="flex-shrink-0 w-full h-auto">
+                            <Image
+                                src={image}
+                                alt={`Slide ${index}`}
+                                pt={{
+                                    image: {
+                                    },
+                                    toolbar: {
+                                        style: { gap: '10px', padding: 64 },
+                                    },
+                                    mask: {
+                                        style: { backgroundColor: 'rgba(0, 0, 0, 0.5)' }
+                                    },
+                                }}
+                                preview
+                            />
+                        </div>
                     ))}
                 </div>
             </div>
+
+            <div className='w-1/6'/>
 
             <button
                 onClick={nextSlide}
