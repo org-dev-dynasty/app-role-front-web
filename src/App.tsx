7b423e59473de0d35a5bdada29a2e30a396005ee
ZettDev <131803879+ZettDev@users.noreply.github.com>
2024-08-08 01:24:33 -0300

    Popup and other sections added

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,14 @@ import { Navbar } from "./components/Navbar";
 import { Section2 } from "./components/Section2.tsx";
 import { SectionContainer } from "./components/SectionContainer";
 import { Overlay } from "./components/Overlay";
+import {Section4} from "./components/Section4.tsx";
+import {Section3} from "./components/Section3.tsx";
+import {Popup} from "./components/PopUp.tsx";
 
 
 export function App() {
   const [showMenuItems, setShowMenuItems] = useState(false);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
 
   const handleMenuClick = () => {
     setShowMenuItems(!showMenuItems);
@@ -16,7 +20,9 @@ export function App() {
 
   return (
     <>
-      <Navbar onClickMenuResponsible={handleMenuClick} openMenu={showMenuItems} />
+      <Navbar onClickMenuResponsible={handleMenuClick} openMenu={showMenuItems} openPopUp={() => setIsPopupOpen(true)}/>
+      <Popup isOpen={isPopupOpen} onClose={() => setIsPopupOpen(false)}/>
+
 
       <main className="bg-gradient-to-r from-dark-purple via-purple via-70% to-light-purple font-nunito">
         {/* SECTION 1 - 3D */}
@@ -24,6 +30,8 @@ export function App() {
           <Header />
         </SectionContainer>
         <Section2 />
+        <Section3/>
+        <Section4 />
         <SectionContainer>
           <Contact />
         </SectionContainer>
