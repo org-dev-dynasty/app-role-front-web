23126a1f77bc25bdc3e005333686548d782027b2
ZettDev <131803879+ZettDev@users.noreply.github.com>
2024-08-08 23:25:12 -0300

    Section 2 spacing fix

--- src/components/Section2.tsx
+++ src/components/Section2.tsx
@@ -30,11 +30,11 @@ export const Section2 = () => {
     };
 
     return (
-        <div className="w-full bg-white justify-center rounded-3xl px-12 py-16">
+        <div className="w-full bg-white justify-center rounded-3xl py-16">
             <h1 className="text-4xl text-center pb-12 text-">APROVEITE OS <span className="font-bold text-light-purple">ROLE</span>s COM QUALIDADE</h1>
 
-            <div className="flex flex-row max-lg:hidden">
-                <div className="w-1/3">
+            <div className="flex flex-row max-lg:hidden px-12">
+                <div className="w-1/3 my-auto">
                     <div className={getClassName(0)} onClick={() => setCurrentIndex(0)}>
                         <div className="flex flex-row">
                             <CheckCircle size={36} className="text-light-purple mr-1" />
@@ -56,7 +56,7 @@ export const Section2 = () => {
                     <Carousel currentIndex={currentIndex} nextSlide={nextSlide} prevSlide={prevSlide}/>
                 </div>
 
-                <div className="w-1/3">
+                <div className="w-1/3 my-auto">
                     <div className={getClassName(2)} onClick={() => setCurrentIndex(2)}>
                         <div className="flex flex-row">
                             <Heart size={36} className="text-light-purple mr-1" />
@@ -75,7 +75,7 @@ export const Section2 = () => {
                 </div>
             </div>
 
-            <div className="lg:hidden ">
+            <div className="lg:hidden mx-12">
                 <div className="flex flex-row justify-center items-center">
                     <Carousel currentIndex={currentIndex} nextSlide={nextSlide} prevSlide={prevSlide}/>
 
@@ -83,7 +83,7 @@ export const Section2 = () => {
 
                 <div className={hide(0)}>
                     <div className="flex flex-row items-center justify-center">
-                        <CheckCircle size={48} className="text-light-purple mr-1"/>
+                        <CheckCircle size={48} className="text-light-purple mr-1 flex-shrink-0"/>
                         <p className="text-3xl font-nunito text-light-purple font-medium">RECOMENDAÇÃO</p>
                     </div>
                     <p className="text-2xl font-nunito text-center">Recomendação assertiva dos seus tipos de <span
@@ -92,7 +92,7 @@ export const Section2 = () => {
 
                 <div className={hide(1)}>
                     <div className="flex flex-row items-center justify-center">
-                        <MagnifyingGlass size={48} className="text-light-purple mr-1"/>
+                        <MagnifyingGlass size={48} className="text-light-purple mr-1 flex-shrink-0"/>
                         <p className="text-3xl font-nunito text-light-purple font-medium">PESQUISA</p>
                     </div>
                     <p className="text-2xl font-nunito text-center">Pesquisa avançada com diversos filtros e parâmetros
@@ -102,7 +102,7 @@ export const Section2 = () => {
 
                 <div className={hide(2)}>
                     <div className="flex flex-row items-center justify-center">
-                        <Heart size={48} className="text-light-purple mr-1"/>
+                        <Heart size={48} className="text-light-purple mr-1 flex-shrink-0"/>
                         <p className="text-3xl font-nunito text-light-purple font-medium">SOCIAL</p>
                     </div>
                     <p className="text-2xl font-nunito text-center">Siga pessoas, faça amizades e descubra os usuários que
@@ -111,7 +111,7 @@ export const Section2 = () => {
 
                 <div className={hide(3)}>
                     <div className="flex flex-row items-center justify-center">
-                        <Medal size={48} className="text-light-purple mr-1"/>
+                        <Medal size={48} className="text-light-purple mr-1 flex-shrink-0"/>
                         <p className="text-3xl font-nunito text-light-purple font-medium">BENEFÍCIOS</p>
                     </div>
                     <p className="text-2xl font-nunito text-center">Confirme sua presença, mostre para seus amigos qual
