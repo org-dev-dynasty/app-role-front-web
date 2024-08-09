4029d08c1f3844449cc2b0b94cc1f4db109777e8
Rodrigo Diana Siqueira <rodrigo.dsiqueira1@gmail.com>
2024-08-09 11:36:46 -0300

    chore: added s3 and cloudfront distro

---
+++ iac/iac/iac_stack.ts
@@ -0,0 +1,46 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class IacStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const s3AssetsBucket = new cdk.aws_s3.Bucket(this, 'AppRoleFrontS3AssetsBucket', {
+      bucketName: 'app-role-front-assets-bucket',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      publicReadAccess: true,
+    })
+
+    const distribution = new cdk.aws_cloudfront.CloudFrontWebDistribution(this, 'AppRoleFrontDistribution', {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: s3AssetsBucket,
+          },
+          behaviors: [
+            {
+              isDefaultBehavior: true,
+            }
+          ]
+        }
+      ],
+    })
+
+    distribution.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
+
+    new cdk.CfnOutput(this, 'AppRoleFrontDistributionOutput', {
+      value: distribution.distributionDomainName,
+      description: 'The distribution domain',
+      exportName: 'AppRoleFrontDistributionOutput'
+    });
+
+    new cdk.CfnOutput(this, 'AppRoleFrontS3AssetsBucketOutput', {
+      value: s3AssetsBucket.bucketName,
+      description: 'The S3 bucket name',
+      exportName: 'AppRoleFrontS3AssetsBucketOutput'
+    });
+    
+    
+  }
+}
