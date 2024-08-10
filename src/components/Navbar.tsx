d8916385e30a9baa8a77455e3236f4a39ad3d0cb
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-08-10 13:11:08 -0300

    feat: Add "Quem Somos" to the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,10 +10,11 @@ export function Navbar({ onClickMenuResponsible, openMenu, isAnimating, openPopU
     <div className={`w-screen h-16 ${openMenu ? 'bg-transparent': 'bg-[rgba(29,29,29,0.4)] backdrop-blur rounded-b-2xl'} flex rounded-b-3xl fixed top-0 z-40 font-nunito font-medium text-xl`}>
       <img src={"https://d1jwplf6mvpxl3.cloudfront.net/approle_logo_navbar.png"} alt="AppRole Logo" className={`h-10 ml-8 mt-3 flex items-center justify-center ${openMenu ? 'hidden': ''}`} />
       <div className='flex m-4 justify-center gap-x-12 w-full max-lg:hidden'>
+        <NavbarItem onClick={() => handleScroll('quem_somos')}>Quem Somos</NavbarItem>
         <NavbarItem onClick={() => handleScroll('beneficios')}>Benefícios</NavbarItem>
         <NavbarItem onClick={() => handleScroll('explorar')}>Explorar</NavbarItem>
         <NavbarItem onClick={() => handleScroll('organizador')}>Organizador</NavbarItem>
-        <NavbarItem onClick={() => handleScroll('contato')}>Contato</NavbarItem>
+        <NavbarItem className='md:mr-20' onClick={() => handleScroll('contato')}>Contato</NavbarItem>
         <button onClick={openPopUp} className='
         w-[128px] text-xl
         bg-gradient-to-r from-dark-purple via-purple via-70% to-light-purple
