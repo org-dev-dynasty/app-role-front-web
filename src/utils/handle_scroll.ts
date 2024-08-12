95cba9cc1abd449a4191e487b0be18bc4923f714
ZettDev <131803879+ZettDev@users.noreply.github.com>
2024-08-12 20:07:56 -0300

    scroll animation fix

--- src/utils/handle_scroll.ts
+++ src/utils/handle_scroll.ts
@@ -1,7 +1,11 @@
 export function handleScroll(id: string) {
   const section = document.getElementById(id);
-  console.log('HANDLE SCROL' + section);
+
   if (section) {
-    section.scrollIntoView({ behavior: 'smooth' });
+    const sectionPosition = section.offsetTop;
+    window.scrollTo({
+      top: sectionPosition,
+      behavior: 'smooth',
+    });
   }
 }
