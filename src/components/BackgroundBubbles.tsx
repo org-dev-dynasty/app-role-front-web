5bb80a02973200d03daa0be11cf71aa330f6105b
gabrielmerola <gabrielmerola12@gmail.com>
2024-08-12 11:41:10 -0300

    adjusting width and height

--- src/components/BackgroundBubbles.tsx
+++ src/components/BackgroundBubbles.tsx
@@ -4,10 +4,10 @@ export function BackgroundBubbles({ isNavbarOpened, isPopUpOpened }: { isNavbarO
     background: gradient,
     backdropFilter: 'blur(100px)', // Ajuste o valor do desfoque conforme necessário
     overflow: 'hidden',
-    maxWidth: '100%',
+    maxWidth: '100vw',
   };
   return (
-    <div style={style} className={`${isNavbarOpened || isPopUpOpened ? 'hidden' : ''} animate-slideandSpinInfiniteRightToLeft w-[32%] rounded-full h-[58%] absolute z-0 shadow-bubbleShadow opacity-50 blur-xl max-sm:w-[60%] max-sm:h-[30%] max-lg:w-[38%] max-lg:h-[30%]`}></div>
+    <div style={style} className={`${isNavbarOpened || isPopUpOpened ? 'hidden' : ''} animate-slideandSpinInfiniteRightToLeft w-[32vw] rounded-full h-[58vw] absolute z-0 shadow-bubbleShadow opacity-50 blur-xl max-sm:w-[60vw] max-sm:h-[30vw] max-lg:w-[38vw] max-lg:h-[30vw]`}></div>
   );
 }
 // bg-gradient-to-tr from-dark-purple via-purple via-70% to-light-purple
