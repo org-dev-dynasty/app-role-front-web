c7c821a7da1c4f0ccdfce90a610ad033c16cae7a
gabrielmerola <gabrielmerola12@gmail.com>
2024-08-12 12:36:23 -0300

    adjust width and height bubble

--- src/components/BackgroundBubbles.tsx
+++ src/components/BackgroundBubbles.tsx
@@ -2,13 +2,10 @@ export function BackgroundBubbles({ isNavbarOpened, isPopUpOpened }: { isNavbarO
   const gradient = 'linear-gradient(180deg, #F2E3FC 0%, #9C4EDC 25%, #5A189A 50%, #3C096C 75%, #10002B 100%)';
   const style = {
     background: gradient,
-    backdropFilter: 'blur(100px)', // Ajuste o valor do desfoque conforme necessário
-    overflow: 'hidden',
+    backdropFilter: 'blur(100px)',
     maxWidth: '100vw',
   };
   return (
-    <div style={style} className={`${isNavbarOpened || isPopUpOpened ? 'hidden' : ''} animate-slideandSpinInfiniteRightToLeft w-[32vw] rounded-full h-[58vw] absolute z-0 shadow-bubbleShadow opacity-50 blur-xl max-sm:w-[60vw] max-sm:h-[30vw] max-lg:w-[38vw] max-lg:h-[30vw]`}></div>
+    <div style={style} className={`overflow-x-hidden ${isNavbarOpened || isPopUpOpened ? 'hidden' : ''} animate-slideandSpinInfiniteRightToLeft w-[24vw] h-[24vw] absolute z-0 shadow-bubbleShadow opacity-50 blur-xl transform translate-x-[50vw] rounded-full overflow-hidden max-sm:w-[60vw] max-sm:h-[60vw] max-sm:translate-x-[30vw] max-lg:w-[38vw] max-lg:h-[38vw] max-lg:translate-x-[20vw]`}></div>
   );
 }
-// bg-gradient-to-tr from-dark-purple via-purple via-70% to-light-purple
-// mobile: w-[80%] h-[36%]
