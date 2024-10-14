07b8633abe8f2eb7007fb3c77009a89ee56eb45d
LucaPinheiro <lucapgomes11@gmail.com>
2024-10-13 23:20:02 -0300

    Fix missing newline at the end of SectionContainer component

--- src/components/SectionContainer.tsx
+++ src/components/SectionContainer.tsx
@@ -3,5 +3,6 @@ import { PropsWithChildren } from "react";
 export function SectionContainer({ children, id }: PropsWithChildren & { id?: string }) {
   return <section id={id} className="flex justify-around items-center py-16 text-white max-lg:flex-col">
   {children}
+  
  </section>
 }
