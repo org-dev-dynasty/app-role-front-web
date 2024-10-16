1f460f6c0ee62905b810bbf8250709fd0c28b1c1
Diego Pinheiro Gomes <didipinheiro@outlook.com>
2024-10-16 15:57:16 -0300

    refactor: Remove background color from EventInfoUnit component

--- src/components/EventInfoUnit.tsx
+++ src/components/EventInfoUnit.tsx
@@ -6,7 +6,7 @@ interface EventInfoUnitProps {
 
 export function EventInfoUnit({ label, children }: EventInfoUnitProps) {
   return (
-    <div className="bg-[#2A2A2A] flex flex-col gap-2 mx-2 text-2xl rounded-lg">
+    <div className="flex flex-col gap-2 mx-2 text-2xl rounded-lg">
       <p className="text-center">{label}</p>
 
       <span className="bg-grayModal px-6 py-4 rounded-lg text-center text-stone-300 w-80 h-16 overflow-y-auto">{children}</span>
