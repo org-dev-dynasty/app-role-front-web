f55ac69743cf2b84b1198bc4e07ad2ae78ea45dd
tokujiTO <tiagomassuda123@gmail.com>
2025-01-29 11:05:48 -0300

    feat: third try on doing requests

--- src/utils/envs.ts
+++ src/utils/envs.ts
@@ -1,6 +1,7 @@
 export const envs = {
   cloudfrontUrl: import.meta.env.VITE_CLOUDFRONT_URL as string,
   googleMapsUrl: import.meta.env.VITE_GOOGLEMAPS_URL as string,
+  api: import.meta.env.VITE_API_URL as string,
 };
 
 export const createTokenName = (tokenName: string) => {
